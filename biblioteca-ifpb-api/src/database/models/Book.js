d9539a72cbb3dd710b3a83e7dce91b63ecee5c4d
FranciscoHonorat <jeffhonorato230@gmail.com>
2025-09-10 21:48:02 -0300

    mudança no models book

--- biblioteca-ifpb-api/src/database/models/Book.js
+++ biblioteca-ifpb-api/src/database/models/Book.js
@@ -13,17 +13,79 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Book.hasMany(models.Loan, {
         foreignKey: 'bookId',
-        as: ''
+        as: 'loans'
       });
     }
   }
   Book.init({
-    title: DataTypes.STRING,
-    author: DataTypes.STRING,
-    publisher: DataTypes.STRING,
-    category: DataTypes.STRING,
-    status: DataTypes.STRING,
-    version: DataTypes.STRING
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'O livro precisa ter um título'},
+        notEmpty: { msg: 'O título não pode ser vazio'},
+        len: {
+          args: [1, 255],
+          msg: 'O título deve ter pelo menos 1 caractere!'
+        }
+      }
+    },
+    author: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'O livro precisa ter um autor'}
+      }
+
+    },
+    publisher: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'O livro precisa ter uma data de publicação'},
+        isDate: { msg: 'Data de publicação inválida'},
+      }
+    },
+    category: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: 'O livro precisa ter uma categoria'},
+        notEmpty: { msg: 'A categoria não pode ser vazia'},
+        len: {
+          args: [3, 100],
+          msg: 'A categoria deve ter entre 3 e 100 caracteres!'
+        }
+      }
+    },
+    status: {
+      type: DataTypes.ENUM('Disponível', 'Emprestado', 'Reservado', 'Indisponível'),
+      allowNull: false,
+      defaultValue: 'Disponível',
+      validate: {
+        isIn: {
+          args: [['Disponível', 'Emprestado', 'Reservado', 'Indisponível']],
+          msg: 'Status deve ser Disponível, Emprestado, Reservado ou Indisponível'
+        }
+      }
+    },
+    version: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+      defaultValue: 1.0,
+      validate: {
+        isFloat: { msg: 'A versão deve ser um número decimal'},
+        min: {
+          args: [1.0],
+          msg: 'A versão deve ser no mínimo 1.0'
+        },
+        max: {
+          args: [10.0],
+          msg: 'A versão deve ser no máximo 10.0'
+        }
+      }
+    },
+
   }, {
     sequelize,
     modelName: 'Book',
